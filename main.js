418ddd7439182df15f7465f6f50f8a8c2036d72e
Marce882 <clicksistemas@live.com>
2022-06-28 09:53:42 -0300

    second commit

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
 function saludar(params) {
     
 }
+
+// Destinos
+
+let destino = prompt(`Ingrese el codigo de su destino
+                        - Cañuelas (CAN)
+                        - Capital Federal (CABA)`)
+destino = destino.toLowerCase()
+
+if (destino === "can") {
+    alert ("eligió Cañuelas")
+} else if (destino=== "caba") {
+    alert ("eligió Capital Federal")
+
+}else{
+    alert (`Debe seleccionar entre la opciones disponibles
+                - CAN 
+                - CABA`)
+}
